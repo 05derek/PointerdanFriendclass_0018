96b0b377aaf4bcd425f40696b9bf972d770c20a2
05derek <derek.dzakir.ft24@mail.umy.ac.id>
2025-05-05 11:02:44 +0700

    class orang (friend class 1)

--- pointerDua.cpp
+++ pointerDua.cpp
@@ -1,4 +1,15 @@
+#include <iostream>
+using namespace std;
 
+class mahasiswa
+{
+public:
+    int nim;
+    void showNim()
+    {
+        cout << "no Induk = " << nim << endl;
+    }
+};
 
 int main()
 {
