72078d61599db27242b6ac35abfa0dfcecc1069d
05derek <derek.dzakir.ft24@mail.umy.ac.id>
2025-05-05 11:29:00 +0700

    int main (friend function satu)

--- friendFunctionsatu.cpp
+++ friendFunctionsatu.cpp
@@ -1,18 +1,8 @@
-#include <iostream>
-#include <string>
-using namespace std;
 
-class mahasiswa
+
+int main()
 {
-private:
-    string nama;
-
-public:
-    friend void setNama(mahasiswa &a, string);
-};
-
-void setNama(mahasiswa &a, string b)
-{
-    a.nama = b;
-    cout << a.nama;
+    mahasiswa joko;
+    setNama(joko, "kairi kumar");
+    return 0;
 }
