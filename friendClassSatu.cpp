cbbe02347b3175c5fd4b0b03dc8ef85163898d48
05derek <derek.dzakir.ft24@mail.umy.ac.id>
2025-05-05 11:11:35 +0700

    menampilkan

--- friendClassSatu.cpp
+++ friendClassSatu.cpp
@@ -1,12 +1,14 @@
 
 
-class siswa
+void siswa :: displayALL(orang &a)
 {
-private:
-    int id;
-
-public:
-    void setId(int pId);
-    void displayALL(orang &a);
-};
-
+    cout << id << endl << a.nama;
+}
+void orang :: setNama(string pNama)
+{
+    nama = pNama;
+}
+void siswa :: setId(int pId)
+{
+    id = pId;
+}
