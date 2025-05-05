0b6fd253b51197abb2e4700d78cbfe107c522de9
05derek <derek.dzakir.ft24@mail.umy.ac.id>
2025-05-05 10:50:55 +0700

    contoh pointer 2

--- pointerSatu.cpp
+++ pointerSatu.cpp
@@ -1,4 +1,13 @@
+#include <iostream>
+using  namespace std;
 
+class mahasiswa{
+    public:
+    int nim;
+    void showNim(){
+        cout << "No induk = " <<nim<<endl;
+    }
+};
 
 int main(){
 
