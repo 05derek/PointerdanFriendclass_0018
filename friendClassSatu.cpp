dd76b18073744f1310390c9d0e1a1201974fa1af
05derek <derek.dzakir.ft24@mail.umy.ac.id>
2025-05-05 11:16:49 +0700

    int main pada (friendclassSatu)

--- friendClassSatu.cpp
+++ friendClassSatu.cpp
@@ -1,14 +1,10 @@
 
-
-void siswa :: displayALL(orang &a)
+int main()
 {
-    cout << id << endl << a.nama;
-}
-void orang :: setNama(string pNama)
-{
-    nama = pNama;
-}
-void siswa :: setId(int pId)
-{
-    id = pId;
+    orang joko;
+    joko.setNama("joko susilo");
+    siswa joko_siswa;
+    joko_siswa.setId(1);
+    joko_siswa.displayALL(joko);
+    return 0;
 }
